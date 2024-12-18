1c7897dee1565ab0992ca0facaf35c88182e12b4
bassibulederrickmukasa <62415713+bassibulederrickmukasa@users.noreply.github.com>
2024-12-19 01:54:32 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -10,8 +10,8 @@ require 'vendor/autoload.php'; // Adjust the path if necessary
 use Supabase\SupabaseClient;
 
 // Supabase configuration constants
-define('SUPABASE_URL', 'https://eainxuivwxjmctkwstmq.supabase.co'); // Your Supabase URL
-define('SUPABASE_KEY', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhaW54dWl2d3hqbWN0a3dzdG1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQwNDU2MjgsImV4cCI6MjA0OTYyMTYyOH0.kDET7JoEJFaiQqWaimfCkp-F1pYshNbXZGkJW6bz58E'); // Your Supabase anon key
+define('SUPABASE_URL', 'https://db.eainxuivwxjmctkwstmq.supabase.co'); // Your Supabase URL
+define('SUPABASE_KEY', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhaW54dWl2d3hqbWN0a3dzdG1xIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNDA0NTYyOCwiZXhwIjoyMDQ5NjIxNjI4fQ.P_heEvjHfg18lovtM2yHdq43BBwSpYHNCGea_9Uc_Lg'); // Your Supabase anon key
 
 // Initialize Supabase client
 $supabase = new SupabaseClient(SUPABASE_URL, SUPABASE_KEY);
